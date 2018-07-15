0c036a82383b615fa5e8befa80e0df989729bd9e
fat <rebornhavei@outlook.com>
2018-07-15 12:34:50 +0800

    change to java10 add a batchassertion class

--- src/test/java/com/fat/display/DisplayApplicationTests.java
+++ src/test/java/com/fat/display/DisplayApplicationTests.java
@@ -1,13 +1,14 @@
 package com.fat.display;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,12 +17,22 @@ public class DisplayApplicationTests {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private BatchAssertionDelegator rh;
+
     @Test
     public void contextLoads() {
-        String url = "/word/haha";
-        String res = restTemplate.postForObject(url, "", String.class);
+//        String url = "/word/haha";
+//        String res = restTemplate.postForObject(url, "", String.class);
+//        Assertions.assertThat("aha").isEqualTo(res);
+//        rh.getUrl("/","good");
+//        System.out.println("nice");
+        Map<String, String> resMap = Map.of(
+                "/","good",
+                "/tt", "nice"
 
-        Assertions.assertThat("aha").isEqualTo(res);
+        );
+        rh.getUrls(resMap);
     }
 
 }
